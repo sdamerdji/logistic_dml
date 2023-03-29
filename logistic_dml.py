f64afbee2d72f4c93888bac0d73ca1c845b68172
Salim Damerdji <31995532+sdamerdji@users.noreply.github.com>
2023-03-29 12:56:32 +0100

    Unit tests for logit() and L()

--- logistic_dml.py
+++ logistic_dml.py
@@ -1,52 +1,35 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import numpy as np
 
 
 def L(R, C, givenEstimator, Ctest):
-    Data = pd.concat([R, C], axis=1)
-    Data.columns = ['R'] + list(C.columns)
+    data = pd.concat([pd.Series(R), C], axis=1)
+    data.columns = ['R'] + list(C.columns)
 
     # Using 5-fold to tune parameter for the machine learning model
     tt = len(pd.unique(R))
 
-    # If R is 0/1 variable, transform it into a factor variable
-    if (tt == 2):
-        Data['R'] = np.where(Data['R'] == 0, 'N', 'P')
-        Data['R'] = pd.Categorical(Data['R'], categories=['N', 'P'])
-
     # Fit the model
-    if ((tt == 2) & (givenEstimator in ['svmLinear2'])):
-        Fit = GridSearchCV(estimator=givenEstimator,
-                           param_grid={'C': [0.01, 0.1, 1, 10, 100, 1000]},
-                           cv=5,
-                           verbose=False)
-        Fit.fit(Data.iloc[:, 1:], Data['R'])
+    '''    
+    if tt == 2 & (givenEstimator in ['svmLinear2']):
+            fit = GridSearchCV(estimator=givenEstimator,
+                               param_grid={'C': [0.01, 0.1, 1, 10, 100, 1000]},
+                               cv=5,
+                               verbose=False)
+            fit.fit(data.iloc[:, 1:], data['R'])
+        else:
+    '''
+
+    givenEstimator.fit(data.iloc[:, 1:], data['R'])
+
+    if tt == 2:
+        Rtest = givenEstimator.predict_proba(Ctest)[:, 1]
     else:
-        Fit = givenEstimator.fit(Data.iloc[:, 1:], Data['R'])
+        Rtest = givenEstimator.predict(Ctest)
 
-    if (tt == 2):
-        Rtest = Fit.predict_proba(Ctest)[:, 1]
-    else:
-        Rtest = Fit.predict(Ctest)
-
-    return (Rtest)
-
-
-aBar = np.zeros(nrow(Xtest))
-
-for j in range(1, K + 1):
-    idNj = (I2 != j)
-    idj = (I2 == j)
-
-    # Fit hat_M^{-k,-j} = L(Y,(A,X); {-k,-j})
-    Mp[idj] = L(Y[idNj], np.concatenate((A, X), axis=1)[idNj, :], givenEstimator,
-                np.concatenate((A, X), axis=1)[idj, :])
-
-    # Fit hat_a^{-k,-j} = L(A,X; {-k,-j})
-    atemp = L(A[idNj], X[idNj, :], givenEstimator, np.concatenate((X[idj, :], Xtest), axis=0))
-    ap[idj] = atemp[0:nrow(X[idj, :]), ]
-    aBar = aBar + atemp[nrow(X[idj, :]):, ]
+    return Rtest
 
 
 def Lr0(Y, A, X, K, givenEstimator, Xtest):
@@ -57,7 +40,7 @@ def Lr0(Y, A, X, K, givenEstimator, Xtest):
     # Equation (3.8): aBar:= hat_a^{-k}(X^{k}) = 1/K sum_{j=1}^K hat_a^{-k,-j}(X^{k})
     aBar = aBar / K
 
-    if (np.sum(np.isinf(Mp)) > 0):
+    if np.sum(np.isinf(Mp)) > 0:
         print('Infinite in Mp')
 
     Mp[Mp <= 1e-8] = 1e-8
@@ -101,17 +84,20 @@ def DML(Y, A, X, K, givenEstimator):
 
     return {'mXp': mXp, 'rXp': rXp}
 
+
 def Estimate(Y, A, dml):
     resA = A - dml['mXp']
-    C = np.sum(resA*(1-Y)*np.exp(dml['rXp']))
+    C = np.sum(resA * (1 - Y) * np.exp(dml['rXp']))
+
     def g(beta):
-        return np.sum(Y*np.exp(-beta*A)*resA) - C
+        return np.sum(Y * np.exp(-beta * A) * resA) - C
 
     lo = 0
     up = 2
     beta0 = optimize.root_scalar(g, bracket=[lo, up]).root
 
     return beta0
+
 
 def Bootstrap(Y, A, dml, B=1000):
     resA = A - dml['mXp']
@@ -120,10 +106,8 @@ def Bootstrap(Y, A, dml, B=1000):
         e = np.random.normal(size=len(Y))
         C = np.sum(e * resA * (1 - Y) * np.exp(dml['rXp']))
 
-
         def g(beta):
             return np.sum(e * Y * np.exp(-beta * A) * resA) - C
-
 
         lo = 0
         up = 2
@@ -133,8 +117,9 @@ def Bootstrap(Y, A, dml, B=1000):
     return np.concatenate(
         (np.quantile(Betas[Betas != 0], [0.025, 0.975]), np.mean(Betas[Betas != 0]), np.std(Betas[Betas != 0])))
 
+
 def logit(x):
     x = np.log(x / (1 - x))
     if np.sum(np.isnan(x)) > 0:
-    print('Na in logit!')
+        raise ValueError
     return x
---
+++ test_logistic_dml.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from logistic_dml import *
+
+
+class TestL(unittest.TestCase):
+    def test_L(self):
+        # Test case with binary R
+        R = np.array([0, 1, 1, 0, 1])
+        C = pd.DataFrame({'X1': [1, 4, 7, 10, 13], 'X2': [2, 5, 8, 11, 14], 'X3': [3, 6, 9, 12, 15]})
+        Ctest = pd.DataFrame({'X1': [16, 19], 'X2': [17, 20], 'X3': [18, 21]})
+        model = LogisticRegression(random_state=42)
+        expected = np.array([0.85, 0.9])
+        np.testing.assert_allclose(np.round(L(R, C, model, Ctest), 2), expected, rtol=1e-6)
+
+        # Test case with continuous R
+        R = np.array([0.2, 0.5, 0.8])
+        C = pd.DataFrame({'X1': [1, 3, 5], 'X2': [2, 4, 6]})
+        Ctest = pd.DataFrame({'X1': [7, 9], 'X2': [8, 10]})
+        model = LinearRegression()
+        expected = np.array([1.1, 1.4])
+        np.testing.assert_allclose(np.round(L(R, C, model, Ctest), 2), expected, rtol=1e-6)
+
+
+class TestLogit(unittest.TestCase):
+    def test_logit(self):
+        x = np.array([0.5, 0.25, 0.75])
+        expected = np.array([0, -1.0986122886681098, 1.0986122886681098])
+        np.testing.assert_allclose(logit(x), expected)
+
+        x = np.array([-1])
+        with np.testing.assert_raises(ValueError):
+            logit(x)
+
+        x = np.array([2])
+        with np.testing.assert_raises(ValueError):
+            logit(x)
+
+if __name__ == '__main__':
+    unittest.main()
